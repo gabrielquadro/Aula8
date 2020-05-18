db1f02d07a5dd55f76afe7dd7fbb8e41ca3db854
gabrielquadro <61526044+gabrielquadro@users.noreply.github.com>
2020-05-18 15:14:00 -0300

    Verificação se login e senha são válidos

--- app/src/main/java/com/unisc/aula8/MainActivity.java
+++ app/src/main/java/com/unisc/aula8/MainActivity.java
@@ -30,25 +30,28 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void entrarPressed(View view) {
-        String query = "SELECT * FROM usuario WHERE usuario = "
-                + etusuario.getText().toString();
+        String query = "SELECT * FROM usuario";
 
         SQLiteDatabase db = helper.getReadableDatabase();
         Cursor c = db.rawQuery(query,null);
         c.moveToFirst();
         int count = 0;
         for(int i=0;i< c.getCount(); i++){
-            count ++;
-            String usuario = c.getString(0);
-            String senha = c.getString(1);
+            String usuario = c.getString(1);
+            String senha = c.getString(2);
             if(usuario.equals(etusuario.getText().toString())){
-                Intent intent1 = new Intent(this, Aula8Busca.class);
-                startActivity(intent1);
+                if(senha.equals(etsenha.getText().toString())) {
+                    Intent intent1 = new Intent(this, Aula8Busca.class);
+                    startActivity(intent1);
+                    count ++;
+                }
             }
             c.moveToNext();
         }
+        if(count == 0){
+            Toast.makeText(this, "Login Invalido", Toast.LENGTH_SHORT).show();
+        }
         c.close();
-        Toast.makeText(this,"" + count,Toast.LENGTH_SHORT).show();
     }
 
     public void novoPressed(View view) {
