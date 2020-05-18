80ebe3fa180fc0e37e92e435602d94970554275f
gabrielquadro <61526044+gabrielquadro@users.noreply.github.com>
2020-05-18 19:01:40 -0300

    Busca por modelo ordenada por ano

--- app/src/main/java/com/unisc/aula8/Aula8Busca.java
+++ app/src/main/java/com/unisc/aula8/Aula8Busca.java
@@ -18,7 +18,7 @@ import java.util.Map;
 
 public class Aula8Busca extends AppCompatActivity {
     private ListView lista;
-    private EditText etAno;
+    private EditText etAno,etModelo;
     List<Map<String,Object>> ListaMap;
     private DataBaseHelper helper;
 
@@ -28,8 +28,9 @@ public class Aula8Busca extends AppCompatActivity {
         setContentView(R.layout.activity_aula8_busca);
         helper = new DataBaseHelper(this);
 
-        lista = findViewById(R.id.lista);
-        etAno = findViewById(R.id.anoSelecioando);
+        lista = findViewById(R.id.listView);
+        etAno = findViewById(R.id.etAnoSel);
+        etModelo = findViewById(R.id.etmodeloSel);
     }
 
     public void addPressed(View view) {
@@ -43,6 +44,37 @@ public class Aula8Busca extends AppCompatActivity {
             query = "SELECT * FROM carro";
         }else{
             query = "SELECT * FROM carro WHERE ano = " + etAno.getText().toString();
+        }
+        ListaMap = new ArrayList<>();
+        SQLiteDatabase db = helper.getReadableDatabase();
+        Cursor c = db.rawQuery(query,null);
+        c.moveToFirst();
+        for(int i=0;i< c.getCount(); i++){
+            Map<String,Object> mapa = new HashMap<>();
+            String id = c.getString(0);
+            String modelo = c.getString(1);
+            String ano = c.getString(2);
+            String valor = c.getString(3);
+            mapa.put("id",id);
+            mapa.put("modelo",modelo);
+            mapa.put("ano",ano);
+            mapa.put("valor",valor);
+            ListaMap.add(mapa);
+            c.moveToNext();
+        }
+        c.close();
+        SimpleAdapter adapter = new SimpleAdapter(this,ListaMap,R.layout.lista_item,
+                new String[] {"id","modelo","ano","valor"},
+                new int[] {R.id.tvId,R.id.tvModelo,R.id.tvAno,R.id.tvValor});
+        lista.setAdapter(adapter);
+    }
+
+    public void buscarMOdeloPressed(View view) {
+        String query = "";
+        if(etModelo.getText().toString().isEmpty()){
+            query = "SELECT * FROM carro";
+        }else{
+            query = "SELECT * FROM carro WHERE modelo = '" + etModelo.getText().toString()+"' ORDER BY ano";
         }
         ListaMap = new ArrayList<>();
         SQLiteDatabase db = helper.getReadableDatabase();
